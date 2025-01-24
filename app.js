50629444ad1243e6a35589ae777dcb6c1e7bf8ce
chineloegwu4 <chineloegwu4@gmail.com>
2025-01-24 11:30:05 +0100

    Second Assignment

---
+++ app.js
@@ -0,0 +1,11 @@
+const day = "Friday"
+console.log(day)
+
+
+const position = 5
+console.log(position)
+
+
+const sentence = "It is the 5th day of the week"
+const output = 'Today is friday '+'.'  +  sentence
+console.log(output)
